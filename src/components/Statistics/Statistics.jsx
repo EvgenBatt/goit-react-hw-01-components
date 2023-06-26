cb2722691c0c1609761948b48125b1739445c2ef
EvgenBatt <evgenmgid@gmail.com>
2023-06-26 13:02:05 +0300

    Update Statistics Task

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,23 +2,24 @@ import PropTypes from 'prop-types';
 import { StatisticsItem } from 'components/Statistics/StatisticsItem';
 import css from './Statistics.module.css';
 
-export const Statistics = ({ stats }) => {
+export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>Upload stats</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css['stat-list']}>
-        {stats.map(({ id, percentage, label }) => (
-          <li className={css.item} key={id}>
-            <StatisticsItem percentage={percentage} label={label} />
-          </li>
-        ))}
+        {stats.map(({ id, percentage, label, index }) => (
+            <li className={css.item} key={id} index={index}>
+              <StatisticsItem percentage={percentage} label={label} />
+            </li>
+          ))}
       </ul>
     </section>
   );
 };
 
 Statistics.propTypes = {
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
